c5b9b853cc47a9cacfcbec12595040c6a3b25563
Raedalmasri <lbnalmasriraed@gmail.com>
2020-10-21 12:47:57 +0300

    new update
    new update from raed

--- Assets/Scripts/CoinController.cs
+++ Assets/Scripts/CoinController.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 
 public class CoinController : MonoBehaviour
 {
+    public float speed;
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(new Vector3(0, 90, 0) * Time.deltaTime);
+        transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime * speed);
     }
 }
